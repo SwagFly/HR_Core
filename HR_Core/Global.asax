1625d0c42032e1b6101a138f667c94541fc62180
SwagFly <z65454556556a@qq.com>
2019-06-20 09:41:45 +0800

    第一次合成项目

---
+++ HR_Core/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HR_Core.MvcApplication" Language="C#" %>
---
+++ HR_Core/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HR_Core.Startup))]
+namespace HR_Core
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
